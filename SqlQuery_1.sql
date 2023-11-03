4fcdbfcfe1a15a4ed4f438ef5d477c2874f697f1
Josh Norman <135571751+squishymamafat@users.noreply.github.com>
2023-11-02 22:19:04 -0400

    Add files via upload

---
+++ SqlQuery_1.sql
@@ -0,0 +1,9 @@
+﻿SELECT cust.CustomerName
+FROM orders o
+JOIN customers cust ON o.CustomerID =cust.CustomerID
+WHERE o.OrderID = 10310;
+
+SELECT sup.Address
+FROM products p
+JOIN suppliers sup ON p.SupplierID = sup.SupplierID
+WHERE p.ProductID = 40;
